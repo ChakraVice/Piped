ba988d9dfa2fd9db96732f3646d228fedc643a8c
ChakraVice <157118969+ChakraVice@users.noreply.github.com>
2024-05-10 19:59:13 +0900

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,7 +8,7 @@
                     height="32"
                     width="32"
                     class="mr-[-0.6rem] w-10"
-                />yt</router-link
+                />ㅤ</router-link
             >
         </div>
         <div class="search-container lt-md:hidden">
